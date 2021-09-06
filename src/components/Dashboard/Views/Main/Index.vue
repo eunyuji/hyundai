8e25d2afbd1da5200df7dab2ab7694d3bd265bb0
eunyu <eunyu.ji@emoion.co.kr>
2021-09-06 19:12:50 +0900

    초기화

--- src/components/Dashboard/Views/Main/Index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    메인
-  </div>
-</template>
-<script>
-
-</script>
-<style scoped></style>
--- src/views/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        메메인
-    </div>
-</template>
-<script>
-</script>
-<style scoped></style>
